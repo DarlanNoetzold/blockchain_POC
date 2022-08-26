1da43e2dd1c5629f5b809a5dad62fec60e399f5a
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2022-08-26 06:44:36 -0300

    .

--- contracts/blockchain.sol
+++ contracts/blockchain.sol
@@ -4,7 +4,7 @@ pragma solidity >=0.4.22 <0.9.0;
 contract blockchain {
   string public greetings;
   constructor() public {
-    greetings = "testeee";
+    greetings = "testee";
   }
 
   function sayHello() public view returns(string memory){
